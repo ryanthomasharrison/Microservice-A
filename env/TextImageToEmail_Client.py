c6a5374c0f5e856ce86d59c83d3ff5aa126c4a1d
ryanharrison89 <coby8921@gmail.com>
2024-05-13 16:29:49 -0400

    Successfully tested text and image email

--- env/TextImageToEmail_Client.py
+++ env/TextImageToEmail_Client.py
@@ -14,7 +14,7 @@ def read_file(file_path, file_type):
         raise ValueError("Only text or image file types are supported")
     return data
 
-def CreateJSONtoSend(data, email):
+def CreateJSONtoSend(data, data_type, subject, receiver_email, sender_email = None, sender_password = None):
     # If data to send is text
     if isinstance(data, str):
         data_to_send = data
@@ -28,8 +28,11 @@ def CreateJSONtoSend(data, email):
 
     # Create JSON payload
     payload = {"data": data_to_send, 
-               "dataType": data_type, 
-               "email": email}
+               "data_type": data_type,
+               "subject": subject,
+               "receiver_email": receiver_email,
+               "sender_email": sender_email,
+               "sender_password": sender_password}
 
     # Serialize the JSON payload
     payload_serialized = json.dumps(payload)
@@ -41,20 +44,20 @@ def send_data_to_microservice(json_payload):
         # Set up socket and send data to microservice
         context = zmq.Context()
         socket = context.socket(zmq.REQ)
-        socket.connect("tsp://localhost:5555")
+        socket.connect("tcp://localhost:5555")
         socket.send(json_payload.encode("utf-8"))
-        response = json.loads(socket.recv().decode("uft-8"))
+        response = json.loads(socket.recv().decode("utf-8"))
         socket.close()
 
         # Tell client program whether email was sent successfully or not
-        if response["success"]:
+        if response["success"] == True:
             print("Success! Email was sent")
+            print("error: ", response["error"])
         else:
             print("Error - email did not send. Error message: ", response["error"])
 
     except Exception as error:
         print("Error: ", error)
-
 
 
 
@@ -67,11 +70,14 @@ sample_text = read_file("Sample_data/SampleText.txt", "text")
 
 sample_image = read_file("Sample_data/SampleImage.png", "image")
 
+print("Sending email")
+
 # Convert text to JSON
-text_JSON = CreateJSONtoSend(sample_text, "test@test.com")
+text_JSON = CreateJSONtoSend(sample_text, "text", "test email - text", "ryanharrison.cs361@gmail.com")
 
-# Convert image to JSON
-image_JSON = CreateJSONtoSend(sample_image, "test@test.com")
+# # Convert image to JSON
+image_JSON = CreateJSONtoSend(sample_image, "image", "test email - image", "ryanharrison.cs361@gmail.com")
 
-print(text_JSON)
-
+# Send JSON to microservice
+# send_data_to_microservice(text_JSON)
+# send_data_to_microservice(image_JSON)
--- env/TextImageToEmail_Service.py
+++ env/TextImageToEmail_Service.py
@@ -2,7 +2,9 @@ import base64
 import json
 import zmq
 import smtplib
-import email.mime
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
 
 
 def send_email(sender_email, sender_password, recipient_email, email_subject, email_data, data_type):
@@ -11,27 +13,24 @@ def send_email(sender_email, sender_password, recipient_email, email_subject, em
         server.starttls()
         server.login(sender_email, sender_password)
 
-        mime_email = email.mime.multipart.MIMEMultipart()
+        mime_email = MIMEMultipart()
         mime_email["From"] = sender_email
         mime_email["To"] = recipient_email
         mime_email["Subject"] = email_subject
 
         if data_type == "image":
-            mime_attachment = email.mime.image.MIMEImage(email_data)
+            mime_attachment = MIMEImage(email_data)
         elif data_type == "text":
-            mime_attachment = email.mime.text.MIMEText(email_data)
+            mime_attachment = MIMEText(email_data)
         else:
             raise ValueError("Only text or image file types are supported")
 
         mime_attachment.add_header("Content-Disposition", "attachment")
         mime_email.attach(mime_attachment)
         server.sendmail(sender_email, recipient_email, mime_email.as_string())
-
-        return f"Success! Email has been sent to {recipient_email}"
     
     except Exception as error:
         print(f"Error in sending: {error}")
-        return str(error)
     
     finally:
         server.quit()
@@ -40,25 +39,26 @@ def send_email(sender_email, sender_password, recipient_email, email_subject, em
 # Set up and bind socket
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+address = "tcp://*:5555"
+socket.bind(address)
 
 # Wait for client request
 while True:
+    print("Listening on ", address)
     json_payload = socket.recv_json()
 
     # Extract data from json payload
+    sender_email = json_payload["sender_email"]
+    sender_password = json_payload["sender_password"]
     data = json_payload["data"]
-    data_type = json_payload["dataType"]
-    receiver_email = json_payload["email"]
+    data_type = json_payload["data_type"]
+    receiver_email = json_payload["receiver_email"]
     subject = json_payload["subject"]
     
-    # Get sender email and password if provided, otherwise use default email/password in function call below
-    sender_email = json_payload.get("senderEmail")
-    sender_password = json_payload.get("senderPassword")
-    
+    # Use default email/password if not provided by client in payload
     if sender_email is None and sender_password is None:
         sender_email = "cs361.group55@gmail.com"
-        sender_password = "textimagetoemail"
+        sender_password = "rddv jysq efqu wvaf"
 
     # Decode base64 if an image was provided
     if data_type == "image":
